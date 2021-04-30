9c6600a489d683b2f8ec59dda62a60ffc4295217
Tristan Larson <telarson@selene.beocat.ksu.edu>
2021-04-30 14:04:37 -0500

    OpenMP output tests

--- OpenMP/1k-lines/openmp-sbatch.sh
+++ OpenMP/1k-lines/openmp-sbatch.sh
@@ -1,2 +1,2 @@
-#!/bin/bash -1
-//usr/bin/time -o /homes/telarson/CIS520_Project4/OpenMP/1k-lines/time/time-$RANDOM.txt /homes/telarson/CIS520-Project4/OpenMP/1k-lines/openmp-1k
+#!/bin/bash -l
+//usr/bin/time -o /homes/telarson/CIS520-Project4/OpenMP/1k-lines/time/time-$RANDOM.txt /homes/telarson/CIS520-Project4/OpenMP/1k-lines/openmp-1k
